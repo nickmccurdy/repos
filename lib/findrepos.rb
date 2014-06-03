2c24105b64a72b010d7cd18eea40b785c7d43808
Nicolas McCurdy <thenickperson@gmail.com>
2014-06-03 06:31:14 -0400

    Implement a simple listing of git repos

--- lib/findrepos.rb
+++ lib/findrepos.rb
@@ -1,5 +1,8 @@
 require "findrepos/version"
+require "pathname"
 
 module Findrepos
-  # Your code goes here...
+  def self.list
+    Dir.glob('*/.git').map { |dir| Pathname.new(dir).dirname }
+  end
 end
