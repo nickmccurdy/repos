d99af7902a3d9943d0357f08e19369e0468cd366
Nicolas McCurdy <thenickperson@gmail.com>
2014-06-04 01:50:02 -0400

    Add specs for Findrepos.clean?

--- spec/findrepos_spec.rb
+++ spec/findrepos_spec.rb
@@ -8,18 +8,50 @@ describe Findrepos do
   end
 
   describe '.clean?' do
+    before :example do
+      Dir.mkdir 'repo'
+      Dir.chdir 'repo' do
+        `git init`
+        FileUtils.touch 'file'
+        `git add file`
+        `git commit -m "Initial commit."`
+      end
+    end
+
+    after :example do
+      FileUtils.rm_r 'repo'
+    end
+
     context 'when the given repo has an untracked file' do
-      it 'returns false'
+      it 'returns false' do
+        Dir.chdir('repo') { FileUtils.touch 'README' }
+        expect(Findrepos.clean? 'repo').to be false
+      end
     end
+
     context 'when the given repo has a modified file in the working tree' do
-      it 'returns false'
+      it 'returns false' do
+        Dir.chdir('repo') { `echo "hello" > file` }
+        expect(Findrepos.clean? 'repo').to be false
+      end
     end
+
     context 'when the given repo has a modified file in the stage' do
-      it 'returns false'
+      it 'returns false' do
+        Dir.chdir 'repo' do
+          `echo "hello" > file`
+          `git add file`
+        end
+        expect(Findrepos.clean? 'repo').to be false
+      end
     end
+
     context 'when the given repo has neither uncommitted changes nor untracked files' do
-      it 'returns true'
+      it 'returns true' do
+        expect(Findrepos.clean? 'repo').to be true
+      end
     end
+
     context 'when the given repo has no commits' do
       it 'has tests'
     end
