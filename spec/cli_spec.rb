b7106b91f12601af628c46a7db2070671239235d
Nicolas McCurdy <thenickperson@gmail.com>
2014-06-09 18:39:03 -0400

    Minor reorganization of the CLI spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,25 +1,23 @@
 require 'spec_helper'
 
 describe Findrepos::CLI do
-  let(:cli) { Findrepos::CLI.new }
-
   describe '#list' do
     it 'has tests'
   end
 
   describe '#say_git_status' do
-    context 'by default' do
-      it 'displays a clean repository' do
-        expect do
-          cli.send(:say_git_status, true, 'hello')
-        end.to output("clean hello\n").to_stdout
-      end
+    let(:cli) { Findrepos::CLI.new }
 
-      it 'displays a dirty repository' do
-        expect do
-          cli.send(:say_git_status, false, 'hello')
-        end.to output("dirty hello\n").to_stdout
-      end
+    it 'displays a clean repository' do
+      expect do
+        cli.send(:say_git_status, true, 'hello')
+      end.to output("clean hello\n").to_stdout
+    end
+
+    it 'displays a dirty repository' do
+      expect do
+        cli.send(:say_git_status, false, 'hello')
+      end.to output("dirty hello\n").to_stdout
     end
   end
 end
