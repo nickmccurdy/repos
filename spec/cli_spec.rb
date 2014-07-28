7090b1e404e0cf6800894f7080d24e23b4757c45
Nicolas McCurdy <thenickperson@gmail.com>
2014-07-28 15:40:43 -0400

    Don't use "list" (the default command name) as an argument

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -9,7 +9,7 @@ describe Repos::CLI do
     context 'by default' do
       it 'lists all clean and dirty Git repositories in the current ' \
         'directory' do
-        expect { Repos::CLI.start %w(list dir) }.to \
+        expect { Repos::CLI.start %w(dir) }.to \
           output("clean dir/a_clean_repo\ndirty dir/a_dirty_repo\n")
           .to_stdout
       end
@@ -18,7 +18,7 @@ describe Repos::CLI do
     context 'with --recursive' do
       it 'lists all Git repositories in the current directory and all ' \
       'subdirectories' do
-        expect { Repos::CLI.start %w(list dir --recursive) }.to \
+        expect { Repos::CLI.start %w(dir --recursive) }.to \
           output("clean dir/a_clean_repo\ndirty dir/a_dirty_repo\nclean dir/repo_inside/another_repo\n").to_stdout
       end
     end
@@ -30,7 +30,7 @@ describe Repos::CLI do
     context 'with --names' do
       it 'lists all clean and dirty Git repositories in the current ' \
         'directory' do
-        expect { Repos::CLI.start %w(list dir --names) }.to \
+        expect { Repos::CLI.start %w(dir --names) }.to \
           output("dir/a_clean_repo\ndir/a_dirty_repo\n").to_stdout
       end
     end
@@ -38,14 +38,14 @@ describe Repos::CLI do
     context 'with --filter' do
       context 'when filter is "clean"' do
         it 'only lists clean repositories' do
-          expect { Repos::CLI.start %w(list dir --filter=clean) }.to \
+          expect { Repos::CLI.start %w(dir --filter=clean) }.to \
             output("clean dir/a_clean_repo\n").to_stdout
         end
       end
 
       context 'when filter is "dirty"' do
         it 'only lists dirty repositories' do
-          expect { Repos::CLI.start %w(list dir --filter=dirty) }.to \
+          expect { Repos::CLI.start %w(dir --filter=dirty) }.to \
             output("dirty dir/a_dirty_repo\n").to_stdout
         end
       end
